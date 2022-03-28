c369152ebf476a9ed228e8e7a98179c71d5e3723
Akshdange <101630944+Akshdange@users.noreply.github.com>
2022-03-28 15:55:36 +0530

    Add files via upload

---
+++ Question2.java
@@ -0,0 +1,14 @@
+package selfEvaluationLab1;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		int a=6;
+		int b=7;
+		int sum = a+b;
+		System.out.println("sum="+sum);
+		// TODO Auto-generated method stub
+
+	}
+
+}
